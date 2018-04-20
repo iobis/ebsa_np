017041dde9720ad9ae4cba0c547dbcb3541e6713
Samuel Bosch <mail@samuelbosch.com>
2018-04-20 17:16:17 +0200

    fix issues with geometries, better title

--- scripts/generate_summaries.R
+++ scripts/generate_summaries.R
@@ -5,10 +5,10 @@ library(dplyr)
 generate_ebsa_summaries <- function(overwrite_data=FALSE, overwrite_reports=FALSE) {
   poly <- sf::st_read('data/Global_EBSAs_Automated_Final_1104_2016_WGS84/Global_EBSAs_Automated_Final_1104_2016_WGS84.shp', stringsAsFactors = FALSE)
   ids <- unique(poly$GLOBAL_ID)
-
   for (id in ids) {
     print(id)
     ebsafile <- paste0('data/ebsa_', id, '.rds')
+    ebsaname <- poly$NAME[poly$GLOBAL_ID == id][1]
     if(!file.exists(ebsafile) || overwrite_data) {
       ft <- poly %>% filter(GLOBAL_ID == id)
       info <- ft %>% select(NAME, Workshop, EBSA_ID, AREA_MW_KM, GLOBAL_ID) %>% distinct()
@@ -18,14 +18,20 @@ generate_ebsa_summaries <- function(overwrite_data=FALSE, overwrite_reports=FALS
       geom <- sf::st_geometry(ft)
       geom <- sf::st_cast(geom, 'POLYGON')
       data$geom <- geom
-      convex <- sf::st_convex_hull(geom)
-      convex <- sf::st_buffer(convex, 0.1)
-      convex <- sf::st_simplify(convex, preserveTopology = TRUE, dTolerance = 0.05)
-      data$occ <- bind_rows(lapply(convex, function(poly) {
+
+      geom <- sf::st_convex_hull(geom)
+      geom <- sf::st_buffer(geom, 0.1)
+      geom <- sf::st_simplify(geom, preserveTopology = TRUE, dTolerance = 0.05)
+      geom <- sf::st_intersection(geom, sf::st_as_sfc(sf::st_bbox(data$geom)))
+
+      data$occ <- bind_rows(lapply(geom, function(poly) {
         wkt <- st_as_text(poly)
         occ <- robis2::occurrence(geometry = wkt)
         fields <- c('decimalLongitude', 'decimalLatitude', 'eventDate', 'depth', 'year', 'worms_id', 'valid_id', 'resource_id', 'institutionCode', 'taxonomicgroup')
         if(NROW(occ) > 0) {
+          if(!'depth' %in% colnames(occ)) {
+            occ$depth <- NA
+          }
           if(!all(fields %in% colnames(occ))) {
             warning("Field not found")
             warning(fields[!fields %in% colnames(occ)])
@@ -38,7 +44,7 @@ generate_ebsa_summaries <- function(overwrite_data=FALSE, overwrite_reports=FALS
       # filter occurrences with real geometries
       occ_sp <- sf::st_as_sf(data$occ, coords=c('decimalLongitude', 'decimalLatitude'))
       st_crs(occ_sp) <- st_crs(geom)
-      occ_intersects <- unlist(sf::st_intersects(geom, occ_sp, sparse=TRUE, prepared = TRUE))
+      occ_intersects <- unlist(sf::st_intersects(data$geom, occ_sp, sparse=TRUE, prepared = TRUE))
       data$occ <- data$occ[occ_intersects,]
 
       obisid_table <- table(data$occ$valid_id)
@@ -81,7 +87,7 @@ generate_ebsa_summaries <- function(overwrite_data=FALSE, overwrite_reports=FALS
       saveRDS(data, ebsafile)
     }
     if(!file.exists(paste0('reports/ebsa_', id, '.pdf')) || overwrite_reports) {
-      outputfile <- rmarkdown::render('scripts/summaries.Rmd', output_file  = paste0('ebsa_', id, '.pdf'), output_dir = 'reports', params = list(id=id, ebsafile = paste0('../', ebsafile)))
+      outputfile <- rmarkdown::render('scripts/summaries.Rmd', output_file  = paste0('ebsa_', id, '.pdf'), output_dir = 'reports', params = list(id=id, ebsaname=ebsaname, ebsafile = paste0('../', ebsafile)))
     }
   }
 }
